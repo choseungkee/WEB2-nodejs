370fe4721a4b98fb6a63383868e2432471b22b89
choseungkee <choseungkee@naver.com>
2018-06-18 16:39:10 +0900

    APP제작-글목록 출력하기_20180618

--- main.js
+++ main.js
@@ -14,62 +14,79 @@
 
 	if (pathname === '/'){
 	if (queryData.id === undefined){
-    fs.readFile(`./data/${queryData.id}`,'utf8',function(err,description){
-	    var title = "Welcome!";
-	    var description = "Hello, Nodejs !";
-      var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title} </title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+      fs.readdir('./data', function(error, filelist){
+          //console.log(filelist);
+          var title = "Welcome!";
+          var description = "Hello, Node.js !";
+          var list = '<ol>';
+          var i = 0;
+          while (i < filelist.length) {
+            //list = list + `<li>${filelist[i]}</li>`;
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i+1;
+          }
+          list = list +'</ol>';
 
-    response.writeHead(200);
-    response.end(template);
-    });
-
-} else {
-    fs.readFile(`./data/${queryData.id}`,'utf8', function(err,description){
-    //var description = data;
-    var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title} </title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
+          /*
           <ol>
             <li><a href="/?id=HTML">HTML</a></li>
             <li><a href="/?id=CSS">CSS</a></li>
             <li><a href="/?id=JavaScript">JavaScript</a></li>
           </ol>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-    `;
-    response.writeHead(200);
-    response.end(template);
-    });
+          */
+          var template = `
+              <!doctype html>
+              <html>
+              <head>
+                <title>WEB2 - ${title} </title>
+                <meta charset="utf-8">
+              </head>
+              <body>
+                <h1><a href="/">WEB</a></h1>
+                ${list}
+                <h2>${title}</h2>
+                <p>${description}</p>
+              </body>
+              </html>
+              `;
+      response.writeHead(200);
+      response.end(template);
+      });
+
+} else {
+    fs.readdir('./data', function(error, filelist){
+        var list = '<ol>';
+        var i = 0;
+        while (i < filelist.length) {
+          //list = list + `<li>${filelist[i]}</li>`;
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i = i+1;
+        }
+        list = list +'</ol>';
+        fs.readFile(`./data/${queryData.id}`,'utf8', function(err,description){
+        //var description = data;
+        var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB2 - ${title} </title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+        response.writeHead(200);
+        response.end(template);
+        });
+  });
 }} else {
     response.writeHead(404);
     response.end('Not Found');
 }
-
 });
 app.listen(3000);
